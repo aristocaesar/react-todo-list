73c75f81c75efd37992737a7f5b099e635842aef
aristocaesar <hi@aristoc.space>
2022-01-02 13:29:08 +0700

    start project and add some component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div className="card">
+        <h2>Items</h2>
+        <div className="actions">
+          <button className="btn">Remove</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
